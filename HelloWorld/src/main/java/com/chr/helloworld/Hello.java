9466f603955ad7b4c76e2333fdb8c10d84eebe4d
coreyhope-russo <you@coreyhoperusso@yahoo.ca>
2021-09-28 11:34:11 -0400

    adding hello world project

---
+++ HelloWorld/src/main/java/com/chr/helloworld/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *@author 
+ *email: your email@domain.com
+ *date: current date
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.chr.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Corey!");
+    }
+}
